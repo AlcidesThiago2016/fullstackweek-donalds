96c7f045b0ff59da5c93cb6a80fbdc512e1b1cc2
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2025-02-25 23:05:01 -0300

    Update Project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,5 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-
 const HomePage = () => {
-  return (
-    <div className="p-5 border border-red-500 rounded-xl">
-      <h1 className="p-5">Hello Word</h1>
-      <Button>FSW 7.0</Button>
-      <Input />
-    </div>
-  )
-
+  return <h1 className="p-5">Hello Word</h1>
 };
 
 export default HomePage;
